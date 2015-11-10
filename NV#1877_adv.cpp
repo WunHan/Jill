37b59d42cfe564dde279b41c5279bf6af462ed82
victorhung777 <victorhung777@gmail.com>
2015-11-10 19:15:43 +0800

    [NV#1877] Refine this program 1. Add band_lut structure for easier maintainance 2. Add input_check mechanism 3. Add quit option

--- NV#1877_adv.cpp
+++ NV#1877_adv.cpp
@@ -1,69 +1,101 @@
+/*===========================================================================
+                        EDIT HISTORY FOR MODULE
+  This section contains comments describing changes made to the module.
+  Notice that changes are listed in reverse chronological order.
+when        who       what, where, why
+--------   ------     ----------------------------------------------------------
+11-10-15   Jill       Init version
+11-10-15   Victor     Refine this program
+                      1. Add band_lut structure for easier maintainance
+                      2. Add input_check mechanism
+                      3. Add quit option
+                       
+===========================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#define MAX 8 //the number of channel-1
+#include <ctype.h>
+typedef  unsigned long long uint64;
+#define STRING_LEN  32
+
+
+#define GSM_DCS_1800      (1UL <<  7)
+#define GSM_EGSM_900      (1UL <<  8)
+#define GSM_PGSM_900      (1UL <<  9)
+#define GSM_850           (1UL << 19)
+#define GSM_PCS_1900      (1UL << 21)
+#define WCDMA_I_IMT_2000  (1UL << 22)
+#define WCDMA_II_PCS_1900 (1UL << 23)
+#define WCDMA_IV_1700     (1UL << 25)
+#define WCDMA_V_850       (1UL << 26)
+#define WCDMA_VIII_900    (1UL << 49)
+#define WCDMA_IX_1700     (1UL << 50)
+
+
+typedef struct
+{
+  char name[32];
+  uint64 value;
+}band_info_type;
+
+band_info_type band_lut[] =
+{
+  { "GSM_DCS_1800",       GSM_DCS_1800},
+  { "GSM_EGSM_900",       GSM_EGSM_900},
+  { "GSM_PGSM_900",       GSM_PGSM_900},
+  { "GSM_850",            GSM_850},
+  { "GSM_PCS_1900",       GSM_PCS_1900},
+  { "WCDMA_I_IMT_2000",   WCDMA_I_IMT_2000},
+  { "WCDMA_II_PCS_1900",  WCDMA_II_PCS_1900},
+  { "WCDMA_IV_1700",      WCDMA_IV_1700},
+  { "WCDMA_V_850",        WCDMA_V_850},
+  { "WCDMA_VIII_900",     WCDMA_VIII_900},
+  { "WCDMA_IX_1700",      WCDMA_IX_1700},
+};
+
 
 int main(void)
 {
-	int i,sh,sum_1=0,sum_2=0;
-	char ch;
-	struct data
+	// int i,sh,sum_1=0,sum_2=0;
+  int i = 0;
+	char answer;
+	char s[STRING_LEN];
+  uint64 output = 0;
+  int is_valid_answer = 0;
+  int quit = 0;
+
+
+  for(i=0; i<sizeof(band_lut); i++)
+  {
+    do // input check
     {
-	    char name[20];
-    	int value;
-    };
-	
-	struct data string[10]=
-	{
-		{"GSM850",7},
-		{"EGSM",8},
-		{"DCS1800",19},
-		{"PCS1900",21},
-		{"WCDMA_B1",22},
-		{"WCDMA_B2",23},
-		{"WCDMA_B4",25},
-		{"WCDMA_B5",26},
-		{"WCDMA_B8",49} 
-	};	
-	
-	for(i=0;i<=MAX;i++)
-	{
-		printf("%s: ",string[i].name);
-		ch=getche();
-        printf("\n");
-        if(string[i].value!=49)
-        {
-        	switch (ch)
-            {
-           	    case 'y':
-         	  	    sh=(1<<string[i].value);
-        	    break;
-        	    case 'n':
-        		    sh=(0<<string[i].value);
-        	    break;
-		    }
-		    if(string[i].value==8)
-		    {
-		    	sh=sh|(1<<9);
-			}
-            sum_1=sum_1|sh; 
-            //printf(" A = %x\n",sum_1);
-		}
-		else
-		{
-			switch (ch)
-            {
-           	    case 'y':
-         	  	    sh=(1<<string[i].value);
-        	    break;
-        	    case 'n':
-        		    sh=(0<<string[i].value);
-        	    break;
-		    }
-            sum_2=sum_2|sh;
-            //printf(" A = %x\n",sum_2);
-		}
-	}
-	printf(" A = %x%08x\n",sum_2,sum_1);	
-	
+      is_valid_answer =1;
+      printf("[%-14s]  Supported? ",band_lut[i].name);
+
+      fgets(s, STRING_LEN, stdin);
+      answer = tolower(s[0]);
+      
+      if(answer=='y')
+      {
+        output |=  band_lut[i].value;
+        is_valid_answer = 1;
+      }
+      else if(answer=='n')
+      {
+        is_valid_answer = 1;
+      }
+      else if(answer=='q')
+      {
+        quit = 1;
+        break;
+      }
+    } while(!is_valid_answer);
+    
+    if(1==quit)
+    {
+      break;
+    }
+  }
+  printf("Vaule: %llu\n", output);
+
 }
