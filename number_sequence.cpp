0b03cbc662754159fb63a1075dc2c5347a8e17e5
Jill Huang <jill_huang@htc.com>
2016-07-05 11:26:45 +0800

    Note: Refine code for two options. One for use different array to arrange, another one for use same array to arrange

--- number_sequence.cpp
+++ number_sequence.cpp
@@ -5,8 +5,23 @@
 int main(void)
 {
 	int array[SIZE]={1,0,0,0,3,7,0,0,0};
+	int temp[SIZE]={0};
 	int i, flag=0;
+    
+    //Option1
+    for (i=0;i<SIZE;i++)
+    {  
+        if (array[i]!=0)
+        {
+            temp[flag]=array[i];
+            flag++;
+        }  
+    }
+    for (i=0;i<SIZE;i++)
+        printf("array after sequence:array[%d]=%d\n", i, temp[i]);
 	
+	/* 
+	//Option2
 	for (i=0;i<SIZE;i++)
 	{		
 		if (array[i]!=0)
@@ -15,11 +30,13 @@ int main(void)
 			flag++;
 			printf("flag=%d\n", flag);
 		}		
-	}
-	
+	}	
 	for(i=flag;i<SIZE;i++)
 	    array[i]=0;
-	
+	    
 	for (i=0;i<SIZE;i++)
 	    printf("array after sequence: array[%d]=%d\n", i, array[i]);
+	*/
+	
+	
 }
